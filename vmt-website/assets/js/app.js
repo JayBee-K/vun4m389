0a2da248e3cd8005cbb48f7811ae4f1e701a0fce
Hoàng Tấn <killerdrug007@gmail.com>
2024-08-23 14:43:58 +0700

    Update

--- vmt-website/assets/js/app.js
+++ vmt-website/assets/js/app.js
@@ -1,27 +1,65 @@
-'use strict';
-export let windowWidth = window.innerWidth;
+;(function ($) {
+	'use strict';
 
-/***
- * Function init swiper & init slider
- * @param elm
- * @param obj
- */
-export let handleSwiper = function (elm, obj = {}) {
-	return new Swiper(elm, {
-		loop: true,
-		speed: 1000,
-		autoplay: {
-			delay: 8000,
-			disableOnInteraction: true,
-		},
-		slidesPerView: 1,
-		...obj
-	});
-}
-window.addEventListener('load', function () {
-	window.addEventListener("resize", () => {
-		windowWidth = window.innerWidth;
-	});
-});
+	let windowWidth = window.innerWidth;
 
+	/***
+	 * Function init swiper & init slider
+	 * @param elm
+	 * @param obj
+	 */
+	let handleSwiper = function (elm, obj = {}) {
+		return new Swiper(elm, {
+			loop: true,
+			speed: 1000,
+			autoplay: {
+				delay: 8000,
+				disableOnInteraction: true,
+			},
+			slidesPerView: 1,
+			...obj
+		});
+	}
 
+	let handleServiceCollapse = function () {
+		const serviceCollapse = $('#serviceCollapse');
+		let serviceCollapseButton = serviceCollapse.find('[data-bs-toggle=collapse]');
+		if (serviceCollapse.length && serviceCollapseButton.length) {
+
+			// Thay đổi collapse
+			serviceCollapseButton.each(function (e) {
+				$(this).on('click', function () {
+					const buttonElm = $(this);
+					const ariaExpanded = buttonElm.attr('aria-expanded');
+					if (ariaExpanded === 'true') {
+						setTimeout(function () {
+							const collapseActive = buttonElm.closest('.service-item')[0];
+							serviceLine.css({
+								top: parseInt(collapseActive.offsetTop) + 'px',
+								height: parseInt(collapseActive.scrollHeight) + 'px',
+							});
+						}, 250)
+					}
+				})
+			})
+
+			// Trạng thái khi load trang
+			const serviceLine = serviceCollapse.find('#serviceLine');
+			if (serviceLine.length) {
+				const collapseActive = serviceCollapse.find('.collapse.show').closest('.service-item')[0];
+				serviceLine.css({
+					top: parseInt(collapseActive.offsetTop) + 'px',
+					height: parseInt(collapseActive.scrollHeight) + 'px',
+				});
+			}
+		}
+	}
+
+	$(function () {
+		$(window).resize(function () {
+			windowWidth = window.innerWidth;
+		});
+
+		handleServiceCollapse();
+	})
+})(jQuery);
