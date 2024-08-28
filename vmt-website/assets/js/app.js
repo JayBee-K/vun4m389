1a8c7a7ec4c56c307e64db8e367f9fbd491620fe
Hoàng Tấn <killerdrug007@gmail.com>
2024-08-28 09:25:25 +0700

    Update

--- vmt-website/assets/js/app.js
+++ vmt-website/assets/js/app.js
@@ -22,15 +22,19 @@
 	}
 
 	let handleServiceCollapse = function () {
+
 		const serviceCollapse = $('#serviceCollapse');
 		let serviceCollapseButton = serviceCollapse.find('[data-bs-toggle=collapse]');
 		if (serviceCollapse.length && serviceCollapseButton.length) {
+			const serviceCollapseImage = $('#serviceCollapseImage');
 
 			// Thay đổi collapse
-			serviceCollapseButton.each(function (e) {
-				$(this).on('click', function () {
+			serviceCollapseButton.each(function () {
+				$(this).on('click', function (e) {
+					e.stopPropagation();
 					const buttonElm = $(this);
 					const ariaExpanded = buttonElm.attr('aria-expanded');
+					const dataImage = buttonElm.attr('data-image');
 					if (ariaExpanded === 'true') {
 						setTimeout(function () {
 							const collapseActive = buttonElm.closest('.service-item')[0];
@@ -38,7 +42,13 @@
 								top: parseInt(collapseActive.offsetTop) + 'px',
 								height: parseInt(collapseActive.scrollHeight) + 'px',
 							});
-						}, 250)
+						}, 250);
+
+						if (serviceCollapseImage.length) {
+							const htmlImage = `<img src="${dataImage}" class="h-100 w-auto mw-100 object-fit-contain" alt="">`
+							serviceCollapseImage.find('img')[0].remove();
+							serviceCollapseImage.append(htmlImage);
+						}
 					}
 				})
 			})
