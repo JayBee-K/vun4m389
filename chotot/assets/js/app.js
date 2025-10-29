2036a91b89438ba2ceb7b91b223fce54e4a7ea61
Hoàng Tấn <thtan0407@gmail.com>
2025-10-29 17:22:02 +0700

    Upte

--- chotot/assets/js/app.js
+++ chotot/assets/js/app.js
@@ -1,1 +1,35 @@
+export function handleCollapseContent() {
+	if ($('.handleCollapseContent').length) {
+		$('.handleCollapseContent').each(function () {
+			const collapseContent = $(this);
+			const collapseContentDesc = collapseContent.find('.handleCollapseContentDesc');
+			const collapseContentToggle = collapseContent.find('.handleCollapseContentToggle button');
+			
+			if (collapseContentDesc.length && collapseContentToggle.length) {
+				const limit = parseInt(collapseContentDesc.attr('data-limit'));
+				const full = collapseContentDesc[0].scrollHeight;
+				
+				if (full > limit) {
+					collapseContentDesc.css({
+						'--limit': limit + 'px',
+						'--full': full + 'px'
+					}).removeAttr('data-limit');
+					
+					collapseContentToggle.click(function () {
+						if (collapseContent.hasClass('is-expanded')) {
+							collapseContent.removeClass('is-expanded')
+							collapseContentToggle.html('Mở rộng');
+						} else {
+							collapseContent.addClass('is-expanded')
+							collapseContentToggle.html('Thu gọn');
+						}
+					});
+				} else {
+					collapseContentToggle.parent().remove();
+				}
+			}
+		})
+	}
+}
 
+handleCollapseContent();
